3f8072d4817851cb197638dc4cca844bdfbb96a2
Jason Lingle <jason@lin.gl>
2014-08-09 16:47:26 -0600

    Add outbox.
    Yay, another smaller component that can be independent of the
    monolith. This will make things a bit simpler, at least.

---
+++ src/libpraefectus/outbox.c
@@ -0,0 +1,257 @@
+/*-
+ * Copyright (c) 2014 Jason Lingle
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the author nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "outbox.h"
+
+typedef struct {
+  praef_hlmsg msg;
+  unsigned refcount;
+  unsigned char data[FLEXIBLE_ARRAY_MEMBER];
+} praef_rc_hlmsg;
+
+typedef struct {
+  unsigned queued_at;
+  praef_rc_hlmsg* msg;
+} praef_mq_entry;
+
+struct praef_mq_s {
+  praef_outbox* outbox;
+  praef_message_bus* bus;
+  const PraefNetworkIdentifierPair_t* unicast;
+  praef_mq_entry* pending;
+  unsigned pending_ix, pending_used, pending_cap;
+  unsigned delay;
+
+  SLIST_ENTRY(praef_mq_s) next;
+};
+
+struct praef_outbox_s {
+  SLIST_HEAD(,praef_mq_s) subscribers;
+
+  praef_hlmsg_encoder* enc;
+  unsigned mtu;
+  praef_instant now;
+  praef_rc_hlmsg* next;
+};
+
+static int praef_mq_enqueue(praef_mq*, praef_rc_hlmsg*);
+
+static praef_rc_hlmsg* praef_rc_hlmsg_new(unsigned mtu) {
+  praef_rc_hlmsg* this = malloc(offsetof(praef_rc_hlmsg, data) + mtu + 1);
+  if (!this) return NULL;
+
+  this->refcount = 1;
+  this->msg.size = mtu+1;
+  this->msg.data = this->data;
+  return this;
+}
+
+static inline void praef_rc_hlmsg_incref(praef_rc_hlmsg* this) {
+  ++this->refcount;
+}
+
+static inline void praef_rc_hlmsg_decref(praef_rc_hlmsg* this) {
+  if (!--this->refcount) free(this);
+}
+
+praef_outbox* praef_outbox_new(praef_hlmsg_encoder* enc,
+                               unsigned mtu) {
+  praef_outbox* this = malloc(sizeof(praef_outbox));
+  if (!this) return NULL;
+
+  this->enc = enc;
+  this->mtu = mtu;
+  SLIST_INIT(&this->subscribers);
+
+  this->next = praef_rc_hlmsg_new(mtu);
+  if (!this->next) {
+    free(this);
+    return NULL;
+  }
+
+  return this;
+}
+
+void praef_outbox_delete(praef_outbox* this) {
+  assert(SLIST_EMPTY(&this->subscribers));
+  praef_hlmsg_encoder_delete(this->enc);
+  if (this->next) free(this->next);
+  free(this);
+}
+
+int praef_outbox_append(praef_outbox* this, const PraefMsg_t* msg) {
+  praef_mq* mq;
+  int ok = 1;
+
+  /* next will be NULL if a prior oom occurred */
+  if (!this->next) return 0;
+
+  if (praef_hlmsg_encoder_append(&this->next->msg, this->enc, msg)) {
+    SLIST_FOREACH(mq, &this->subscribers, next)
+      ok &= praef_mq_enqueue(mq, this->next);
+
+    praef_rc_hlmsg_decref(this->next);
+    this->next = praef_rc_hlmsg_new(this->mtu);
+  }
+
+  return ok && this->next;
+}
+
+int praef_outbox_append_singleton(praef_outbox* this, const PraefMsg_t* msg) {
+  praef_rc_hlmsg* next;
+  praef_mq* mq;
+  int ok = 1;
+
+  next = praef_rc_hlmsg_new(this->mtu);
+  if (!next) return 0;
+
+  praef_hlmsg_encoder_singleton(&next->msg, this->enc, msg);
+  SLIST_FOREACH(mq, &this->subscribers, next)
+    ok &= praef_mq_enqueue(mq, next);
+
+  praef_rc_hlmsg_decref(next);
+
+  return ok;
+}
+
+int praef_outbox_flush(praef_outbox* this) {
+  praef_mq* mq;
+  int ok = 1;
+
+  if (!this->next) return 0;
+
+  if (praef_hlmsg_encoder_flush(&this->next->msg, this->enc)) {
+    SLIST_FOREACH(mq, &this->subscribers, next)
+      ok &= praef_mq_enqueue(mq, this->next);
+
+    praef_rc_hlmsg_decref(this->next);
+    this->next = praef_rc_hlmsg_new(this->mtu);
+  }
+
+  return ok && this->next;
+}
+
+void praef_outbox_set_now(praef_outbox* this, praef_instant now) {
+  praef_hlmsg_encoder_set_now(this->enc, now);
+  this->now = now;
+}
+
+praef_mq* praef_mq_new(praef_outbox* outbox,
+                       praef_message_bus* bus,
+                       const PraefNetworkIdentifierPair_t* unicast) {
+  praef_mq* this = malloc(sizeof(praef_mq));
+  if (!this) return NULL;
+
+  this->outbox = outbox;
+  this->bus = bus;
+  this->unicast = unicast;
+  this->pending_ix = 0;
+  this->pending_used = 0;
+  this->pending_cap = 16;
+  this->delay = 0;
+
+  this->pending = calloc(this->pending_cap, sizeof(praef_mq_entry));
+  if (!this->pending) {
+    free(this);
+    return NULL;
+  }
+
+  SLIST_INSERT_HEAD(&outbox->subscribers, this, next);
+  return this;
+}
+
+void praef_mq_delete(praef_mq* this) {
+  unsigned i;
+
+  for (i = 0; i < this->pending_cap; ++i)
+    if (this->pending[i].msg)
+      praef_rc_hlmsg_decref(this->pending[i].msg);
+
+  SLIST_REMOVE(&this->outbox->subscribers, this, praef_mq_s, next);
+  free(this->pending);
+  free(this);
+}
+
+void praef_mq_set_delay(praef_mq* this, unsigned delay) {
+  this->delay = delay;
+}
+
+static int praef_mq_enqueue(praef_mq* this, praef_rc_hlmsg* msg) {
+  praef_mq_entry* new_entries;
+
+  if (this->pending_used == this->pending_cap) {
+    new_entries = realloc(this->pending,
+                          2 * this->pending_cap * sizeof(praef_mq_entry));
+    if (!new_entries) return 0;
+
+    memset(new_entries + this->pending_cap, 0,
+           this->pending_cap * sizeof(praef_mq_entry));
+    this->pending_ix = this->pending_cap;
+    this->pending_cap *= 2;
+  }
+
+  while (this->pending[this->pending_ix].msg) {
+    ++this->pending_ix;
+    this->pending_ix &= (this->pending_cap - 1);
+  }
+
+  praef_rc_hlmsg_incref(msg);
+  this->pending[this->pending_ix].msg = msg;
+  this->pending[this->pending_ix].queued_at = this->outbox->now;
+  ++this->pending_used;
+  return 1;
+}
+
+void praef_mq_update(praef_mq* this) {
+  unsigned i;
+
+  for (i = 0; i < this->pending_cap; ++i) {
+    if (this->pending[i].msg &&
+        this->pending[i].queued_at + this->delay <= this->outbox->now) {
+      if (this->unicast)
+        (*this->bus->unicast)(this->bus, this->unicast,
+                              this->pending[i].msg->msg.data,
+                              this->pending[i].msg->msg.size-1);
+      else
+        (*this->bus->broadcast)(this->bus,
+                                this->pending[i].msg->msg.data,
+                                this->pending[i].msg->msg.size-1);
+
+      praef_rc_hlmsg_decref(this->pending[i].msg);
+      this->pending[i].msg = NULL;
+      --this->pending_used;
+    }
+  }
+}
---
+++ src/libpraefectus/outbox.h
@@ -0,0 +1,122 @@
+/*-
+ * Copyright (c) 2014 Jason Lingle
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the author nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+#ifndef LIBPRAEFECTUS_OUTBOX_H_
+#define LIBPRAEFECTUS_OUTBOX_H_
+
+#include "hl-msg.h"
+#include "message-bus.h"
+#include "messages/PraefMsg.h"
+#include "messages/PraefNetworkIdentifierPair.h"
+
+/**
+ * An outbox is a front-end to a praef_hlmsg_encoder which distributes
+ * resulting messages to subscribed praef_mq objects.
+ */
+typedef struct praef_outbox_s praef_outbox;
+/**
+ * A praef_mq object sends encoded messages received from a praef_outbox to
+ * which it subscribes over a message bus, either via unicast or
+ * broadcast. Messages may be delayed arbitrary lengths of time.
+ */
+typedef struct praef_mq_s praef_mq;
+
+/**
+ * Constructs a new praef_outbox using the given message encoder.
+ *
+ * @param enc The message encoder to use. This call takes ownership of the
+ * encoder, even if it fails.
+ * @param mtu The maximum message size to expect to encode. This should be the
+ * same mtu used to construct enc.
+ * @return The new outbox, or NULL if insufficient memory is available. In the
+ * latter case, the call deletes the message encoder.
+ */
+praef_outbox* praef_outbox_new(praef_hlmsg_encoder* enc,
+                               unsigned mtu);
+/**
+ * Frees the memory held by the given outbox. The outbox MUST have no
+ * subscribed mq objects.
+ */
+void praef_outbox_delete(praef_outbox*);
+
+/**
+ * Appends an aggregatable message to the outbox.
+ *
+ * @return Whether the call succeeds.
+ */
+int praef_outbox_append(praef_outbox*, const PraefMsg_t*);
+/**
+ * Appends a non-aggregatable message to the outbox.
+ *
+ * @return Whether the call succeeds.
+ */
+int praef_outbox_append_singleton(praef_outbox*, const PraefMsg_t*);
+/**
+ * Ensures all pending aggregatable messages have been encoded and passed to
+ * the subscribed outboxes.
+ *
+ * @return Whether the call succeeds.
+ */
+int praef_outbox_flush(praef_outbox*);
+
+/**
+ * Updates the concept of "now" for the encoder and all subscribed mqs.
+ */
+void praef_outbox_set_now(praef_outbox*, praef_instant);
+
+/**
+ * Creates a new praef_mq subscribed to the given outbox.
+ *
+ * @param bus The message bus to use to send messages from this mq. The caller
+ * must ensure the pointer remains valid for the lifetime of the mq.
+ * @param unicast If non-NULL, the network identifier to perform unicasts
+ * on. The caller must ensure this pointer remains valid for the lifetime of
+ * the mq. If NULL, outgoing messages will be broadcast.
+ * @return The new mq object, which is subscribed to the outbox, or NULL if
+ * insufficient memory was available.
+ */
+praef_mq* praef_mq_new(praef_outbox*,
+                       praef_message_bus* bus,
+                       const PraefNetworkIdentifierPair_t* unicast);
+/**
+ * Frees the memory held by the given mq object and unsubscribes it from the
+ * parent outbox.
+ */
+void praef_mq_delete(praef_mq*);
+/**
+ * Changes the delay, in instants, for messages leaving this mq.
+ *
+ * Changes in delay affect all messages that have not yet been sent.
+ */
+void praef_mq_set_delay(praef_mq*, unsigned);
+/**
+ * Advances the mq the given number of ticks into the future, sending any
+ * messages whose delay has fully elapsed.
+ */
+void praef_mq_update(praef_mq*);
+
+#endif /* LIBPRAEFECTUS_OUTBOX_H_ */
